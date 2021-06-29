3dc79a60b98457dd25ddf97bda56bd7824b1568c
Danila Voronkov <danilavoronkov2002@gmail.com>
2021-06-29 17:39:14 +0300

    first commit

---
+++ js/services.js
@@ -0,0 +1,110 @@
+$(document).ready(function() {
+    $( ".otvet-hran-btn" ).click(function() {
+        $( ".block" ).each(function() {
+          $( this ).removeClass( "active-block" );
+        });
+        $( "button" ).each(function() {
+            $( this ).removeClass( "active-btn" );
+        });
+        $('.otvet-hran').addClass('active-block')
+        $('.otvet-hran-btn').addClass('active-btn')
+    });
+
+    $( ".cross-doking-btn" ).click(function() {
+        $( ".block" ).each(function() {
+          $( this ).removeClass( "active-block");
+        });
+        $( "button" ).each(function() {
+            $( this ).removeClass( "active-btn");
+        });
+        $('.cross-doking').addClass('active-block')
+        $('.cross-doking-btn').addClass('active-btn')
+    });   
+
+    $( ".fulfilment-btn" ).click(function() {
+    $( ".block" ).each(function() {
+        $( this ).removeClass( "active-block");
+    });
+    $( "button" ).each(function() {
+        $( this ).removeClass( "active-btn");
+        });
+    $('.fulfilment').addClass('active-block')
+    $('.fulfilment-btn').addClass('active-btn')
+    });   
+
+    $( ".tog-btn" ).click(function() {
+        $( ".block" ).each(function() {
+          $( this ).removeClass( "active-block");
+        });
+        $( "button" ).each(function() {
+            $( this ).removeClass( "active-btn");
+        });
+        $('.tog').addClass('active-block')
+        $('.tog-btn').addClass('active-btn')
+    });   
+
+    $( ".uvh-btn" ).click(function() {
+        $( ".block" ).each(function() {
+          $( this ).removeClass( "active-block");
+        });
+        $( "button" ).each(function() {
+            $( this ).removeClass( "active-btn");
+        });
+        $('.uvh').addClass('active-block')
+        $('.uvh-btn').addClass('active-btn')
+    });   
+
+    $( ".ah-btn" ).click(function() {
+        $( ".block" ).each(function() {
+          $( this ).removeClass( "active-block");
+        });
+        $( "button" ).each(function() {
+            $( this ).removeClass( "active-btn");
+        });
+        $('.ah').addClass('active-block')
+        $('.ah-btn').addClass('active-btn')
+    });   
+
+    $( ".ug-btn" ).click(function() {
+        $( ".block" ).each(function() {
+          $( this ).removeClass( "active-block");
+        });
+        $( "button" ).each(function() {
+            $( this ).removeClass( "active-btn");
+        });
+        $('.ug').addClass('active-block')
+        $('.ug-btn').addClass('active-btn')
+    });   
+
+    $( ".kz-btn" ).click(function() {
+        $( ".block" ).each(function() {
+          $( this ).removeClass( "active-block");
+        });
+        $( "button" ).each(function() {
+            $( this ).removeClass( "active-btn");
+        });
+        $('.kz').addClass('active-block')
+        $('.kz-btn').addClass('active-btn')
+    });   
+
+    $( ".hng-btn" ).click(function() {
+        $( ".block" ).each(function() {
+          $( this ).removeClass( "active-block");
+        });
+        $( "button" ).each(function() {
+            $( this ).removeClass( "active-btn");
+        });
+        $('.hng').addClass('active-block')
+        $('.hng-btn').addClass('active-btn')
+    });   
+    $( ".hg-btn" ).click(function() {
+        $( ".block" ).each(function() {
+          $( this ).removeClass( "active-block");
+        });
+        $( "button" ).each(function() {
+            $( this ).removeClass( "active-btn");
+        });
+        $('.hg').addClass('active-block')
+        $('.hg-btn').addClass('active-btn')
+    });   
+});
